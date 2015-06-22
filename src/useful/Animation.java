e994a6250b4c30bcc0fee84cf4bc93355abb2149
Mr. Coder <emran.bm@gmail.com>
2015-06-22 17:11:18 +0330

    Start method implemented.

--- src/useful/Animation.java
+++ src/useful/Animation.java
@@ -11,70 +11,86 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 /**
- * for using this class you must place all your images(png type) in a folder and rename them from 1.png ! 
+ * For using this class you must place all your images(png type) in a folder and
+ * rename them from 1.png !
+ * 
  * @author HRM_SHAMS
  * @version 1.1
  */
-public class Animation extends JPanel implements Runnable{
+public class Animation extends JPanel implements Runnable {
 
-	private BufferedImage[] images ;
-	private int imageNumber ;
+	private static final long serialVersionUID = 1L;
+
+	private BufferedImage[] images;
+	private int imageNumber;
 	private int delay;
 	private int counter = 0;
 	private int loopNumber;
+
 	/**
-	 * address is the address of images that starts from src folder ! Example : "/images/explosion/"
+	 * 
 	 * @param address
+	 *            is the address of images that starts from src folder ! Example
+	 *            : "/images/explosion/"
 	 * @param imagesNumber
 	 * @param delay
 	 */
-	public Animation( Point location , Dimension size , String address , int imagesNumber , int delay , int loopNumber)
-	{
+	public Animation(Point location, Dimension size, String address,
+			int imagesNumber, int delay, int loopNumber) {
 		setSize(size);
 		setLocation(location);
 		setOpaque(false);
-		
+
 		this.delay = delay;
 		this.imageNumber = imagesNumber;
 		this.loopNumber = loopNumber;
-		
+
 		images = new BufferedImage[imagesNumber];
-		
-		for (int i=0 ; i<imagesNumber ; i++)
-		{
-			try{
-			images[i] = ImageIO.read(getClass().getResource ( address + (i+1)+".png" ));
-			}catch(IOException e){System.err.println("Error in reading image file");}
+
+		for (int i = 0; i < imagesNumber; i++) {
+			try {
+				images[i] = ImageIO.read(getClass().getResource(
+						address + (i + 1) + ".png"));
+			} catch (IOException e) {
+				System.err.println("Error in reading image file");
+			}
 		}
 
 	}
-	
-	public void paintComponent(Graphics g)
-	{
+
+	/**
+	 * Starts the animation as a thread.
+	 */
+	public void start() {
+		Thread t = new Thread(this);
+		t.start();
+	}
+
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.drawImage(images[counter] , 0, 0 , null);
+		g2d.drawImage(images[counter], 0, 0, null);
 	}
 
 	@Override
-	public void run()
-	{
-		for (int i = 1 ; i<=loopNumber ; i++)
-		{
-		
-		while( counter < imageNumber )
-		{
-		
-		repaint();
-		
-		try{
-		Thread.sleep(delay);}catch(Exception e){System.err.println("Error in sleep!");}
+	public void run() {
+		for (int i = 1; i <= loopNumber; i++) {
 
-		counter++;
-		
-		}
-		counter = 0;
-		
+			while (counter < imageNumber) {
+
+				repaint();
+
+				try {
+					Thread.sleep(delay);
+				} catch (Exception e) {
+					System.err.println("Error in sleep!");
+				}
+
+				counter++;
+
+			}
+			counter = 0;
+
 		}
 	}
 }
