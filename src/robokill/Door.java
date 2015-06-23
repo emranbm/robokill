3ac7310da1b04e6bb99dc1d097c374fe43c5fb8b
Mr. Coder <emran.bm@gmail.com>
2015-06-24 02:51:40 +0330

    -A method to get the door status implemented. -Pass to next room implemented.

--- src/robokill/Door.java
+++ src/robokill/Door.java
@@ -4,14 +4,13 @@ import java.awt.Dimension;
 import java.awt.Point;
 
 import useful.Animation;
-import useful.Direction;
 
 /**
  * The door element.
  * 
  * @author Mr. Coder
  * @author HRM_SHAMS
- * @version 1.1
+ * @version 1.3
  */
 public class Door extends Element {
 
@@ -20,6 +19,7 @@ public class Door extends Element {
 	public static final int WIDTH = 50, DEPTH = 10;
 
 	private Animation animation;
+	private boolean isOpened = false;
 
 	/**
 	 * 
@@ -33,14 +33,12 @@ public class Door extends Element {
 	public Door(int x, int y, String type) {
 		super(x, y, 1, 1);
 
-		animation = new Animation(new Point(x,y) , 
-				new Dimension(41,98) ,
-				"/images/doors/" + type + "/" ,
-				5 ,
-				200 ,
-				1 ,
-				false) ; // the size of a door image is 41 x 98
-		
+		animation = new Animation(new Point(x, y), new Dimension(41, 98),
+				"/images/doors/" + type + "/", 5, 200, 1, false); // the size of
+																	// a door
+																	// image is
+																	// 41 x 98
+
 		setSize(animation.getSize());
 		animation.setLocation(0, 0);
 		add(animation);
@@ -50,6 +48,31 @@ public class Door extends Element {
 	 * Just plays the animation and opens the door.
 	 */
 	public void open() {
+		isOpened = true;
 		animation.start();
+	}
+
+	/**
+	 * Determines whether the door is open or closed.
+	 * 
+	 * @return True if the door is open. Otherwise false.
+	 */
+	public boolean isOpen() {
+		return isOpened;
+	}
+
+	/**
+	 * Passes to the next room, if the door is open. Otherwise nothing happens.
+	 * 
+	 * @return Returns true if passed to the next room successfully. Otherwise
+	 *         false.
+	 */
+	public boolean passToNextRoom() {
+		if (!isOpened)
+			return false;
+
+		// TODO Go to next room.
+
+		return true;
 	}
 }
